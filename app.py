2afa734a66ef8414da52471dcc96a9b5f5d22b01
tuancaraballo <tranc2606@gmail.com>
2018-02-25 12:48:17 -0800

    fixing logs

--- app.py
+++ app.py
@@ -64,38 +64,46 @@ def helper(tasks, people, shift):
 @app.route('/test', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        tasks = ''
-        people = ''
-        shift = ''
+        name = ''
         try:
-            tasks = json.loads(request.form['demand'])
-            people = json.loads(request.form['people'])
-            shift = tuple(json.loads(request.form['shift']))
-        except e:
-            raise e
-        logging.debug('People --  %s', str(people))
-        logging.debug('Tasks -- %s', str(tasks))
-        logging.debug('Shift -- %s', str(shift))
-
-        x = helper(tasks, people, shift)
-
-        logging.debug('x -- %s', str(x));
-        result = {}
-        for person in x.keys():
-            person_tasks = []
-            for demand in x[person]:
-                person_tasks.append(demand.demand_name)
-            result[person] = person_tasks
-        print('RESULT', result)
-        logging.warning('Result -- %s', str(result))
-
-        try:
-            result = json.dumps(result)
-        except:
-            print('Error dumping')
-            logging.debug('Error dumping')
-
-        return result
+            name = json.loads(request.form['name'])
+            logging.info('Sucess feetching name %s', name)
+            return 'Success ' + name
+        except Exception as e:
+            logging.warning('Error fetching name from request %s', str(e))
+            raise
+        # tasks = ''
+        # people = ''
+        # shift = ''
+        # try:
+        #     tasks = json.loads(request.form['demand'])
+        #     people = json.loads(request.form['people'])
+        #     shift = tuple(json.loads(request.form['shift']))
+        # except e:
+        #     raise e
+        # logging.debug('People --  %s', str(people))
+        # logging.debug('Tasks -- %s', str(tasks))
+        # logging.debug('Shift -- %s', str(shift))
+        #
+        # x = helper(tasks, people, shift)
+        #
+        # logging.debug('x -- %s', str(x));
+        # result = {}
+        # for person in x.keys():
+        #     person_tasks = []
+        #     for demand in x[person]:
+        #         person_tasks.append(demand.demand_name)
+        #     result[person] = person_tasks
+        # print('RESULT', result)
+        # logging.warning('Result -- %s', str(result))
+        #
+        # try:
+        #     result = json.dumps(result)
+        # except:
+        #     print('Error dumping')
+        #     logging.debug('Error dumping')
+        #
+        # return result
     else:
         logging.warning('Nothing to return')
         return 'nothing to return'
