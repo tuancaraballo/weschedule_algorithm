401231843703b6f2e0633ff3391811fda13d1636
tuancaraballo <tranc2606@gmail.com>
2018-03-05 23:37:38 -0800

    switching back to list

--- app.py
+++ app.py
@@ -87,8 +87,8 @@ def ma_task_handle():
             # logging.debug('Request ma_info -- %s', str(rquest.form['ma_info']))
             # logging.debug('Request task_info -- %s', str(rquest.form['task_info']))
 
-            # ma_info = request.form.getlist('ma_info')
-            ma_info = json.loads(request.form['ma_info'])
+            ma_info = request.form.getlist('ma_info')
+            # ma_info = json.loads(request.form['ma_info'])
             # ma_result = []
             # for res in ma_info:
             #     print('Item from ma info' + str(res))
